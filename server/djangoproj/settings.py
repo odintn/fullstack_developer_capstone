af0b60225e1dc10616de4ece18d9e6d40c40a06e
odintn <nodintsova@optonline.net>
2024-10-24 23:54:40 -0400

    created files for containerization

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://nodintsova-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'https://nodintsova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://nodintsova-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'https://nodintsova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai'
 ]
 
